517c212cb5cfab2100e19c70290a4f9d62555c8b
Zachariah-Farhang <zachariah.farhang@gmail.com>
2023-05-02 23:25:19 +0430

    making the BallPage widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,24 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     const MaterialApp(
-      home: null,
+      home: BallPage() ,
     ),
   );
+}
+
+
+class BallPage extends StatelessWidget {
+  const BallPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: const Text('Ask Me Anything'),
+      ),
+      body: Container(),
+    );
+  }
 }
